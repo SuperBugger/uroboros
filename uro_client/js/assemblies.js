9da7808045a7f498d1905f39bc30df77cd245b35
CASAS <doroshenkoivan71@gmail.com>
2024-07-16 17:58:23 +0300

    the project is now presented on the main page

---
+++ uro_client/js/assemblies.js
@@ -0,0 +1,30 @@
+// assemblies.js
+$(document).ready(function(){
+    var project_name = window.location.pathname.split('/')[1];  // Получение имени проекта из URL
+    $.ajax({
+        url: `http://127.0.0.1:5000/${project_name}/get_assemblies`,  // Используем правильный порт для бэкенда
+        type: 'GET',
+        success: function(response) {
+            if(response.assemblies) {
+                var assemblies = response.assemblies;
+                var tbody = $('#assembliesTable tbody');
+                tbody.empty();
+
+                assemblies.forEach(function(assembly) {
+                    var row = `<tr>
+                        <td>${assembly.assm_id}</td>
+                        <td>${assembly.assm_date_created}</td>
+                        <td>${assembly.assm_desc}</td>
+                        <td>${assembly.assm_version}</td>
+                    </tr>`;
+                    tbody.append(row);
+                });
+            } else {
+                console.error('No assemblies found in response');
+            }
+        },
+        error: function(error) {
+            console.error('Error fetching assemblies:', error);
+        }
+    });
+});
